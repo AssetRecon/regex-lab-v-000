e6d14ff7fbaa9d7c8fbc9ec6014e402b7e2cbf72
Edsel Caprice <caprice@mail.usf.edu>
2018-06-16 19:17:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,10 @@
 def starts_with_a_vowel?(word)
   word.match(/\A[aeiou]/) == word
-    
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+
 end
 
 def words_five_letters_long(text)
